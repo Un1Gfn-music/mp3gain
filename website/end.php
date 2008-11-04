29ea7518c8fed2c0f5a35390ccd8f52f36142967
snelg <>
2008-11-04 01:22:11 +0000

    This commit was manufactured by cvs2svn to create tag 'R1_8'.

--- website/end.php
+++
@@ -1,5 +0,0 @@
-</div>
-<a class="sfLogo" href="http://sourceforge.net">
-<img class="sfLogo" src="http://sourceforge.net/sflogo.php?group_id=49979&amp;type=1" width="88" height="31" border="0" alt="SourceForge.net Logo"/ ></a>
-</body>
-</html>
